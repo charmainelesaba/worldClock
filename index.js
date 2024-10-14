6e9771f944546cc25f9fc3aedb797a36b617a0ee
charmainelesaba <bonelesaba@gmail.com>
2024-10-14 12:42:41 +0200

    Added functionality to select

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function updateTime (){
-    function showCityTime(citySelector, timeSelector, dateSelector, timeZone) {
+    function showCityTime(timeSelector, dateSelector, timeZone) {
+
         let cityTimeElement = document.querySelector(timeSelector);
         let cityDateElement = document.querySelector(dateSelector);
         let cityDate = moment().tz(timeZone).format("dddd, D MMMM, YYYY");
@@ -8,9 +9,31 @@ function updateTime (){
         cityTimeElement.innerHTML = cityTime;
     }
     
-    showCityTime("#first-city", "#first-city-time", "#first-city-date", "Africa/Gaborone");
-    showCityTime("#second-city", "#second-city-time", "#second-city-date", "Pacific/Honolulu");
+    showCityTime("#first-city-time", "#first-city-date", "Africa/Gaborone");
+    showCityTime("#second-city-time", "#second-city-date", "Pacific/Honolulu");
 
 }
 
 setInterval(updateTime, 1000);
+
+
+function showCity(event){
+    let timeZone = event.target.value;
+    let cityTime = moment().tz(timeZone).format("hh:mm [<small>]A[</small>]");
+    let cityDate = moment().tz(timeZone).format("dddd, D MMMM, YYYY");
+    let cityName = timeZone.split("/")[1];
+    let cityElement= document.querySelector("#cities-container");
+    cityElement.innerHTML = `
+    <div class="city" >
+        <div class="city-name-date">
+            <div class="name-of-city">${cityName} </div>
+            <div class="city-date"> ${cityDate}</div>
+        </div>
+        <div class="city-time">${cityTime}</div>    
+    </div>`;
+
+}
+
+
+let citiesSelect= document.querySelector("#select-cities");
+citiesSelect.addEventListener("change", showCity);
